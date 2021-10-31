c38bef027bdd63c980c8d4489125317d606c7063
DarkEye <lesko.matej.pu@gmail.com>
2021-10-31 19:18:22 +0100

    lamp choosing proto flow

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,25 @@
-<script context="module" lang="ts">
-	export const prerender = true;
-</script>
+<script>
+	import { fade } from 'svelte/transition';
 
-<script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	const lamps = ['bedroom', 'proto', 'mock'];
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Lamp Controller Index</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
+<!-- check for inspiration n.6 https://1stwebdesigner.com/free-code-snippets-css-buttons/ -->
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+<div transition:fade>
+	{#each lamps as lamp}
+		<a sveltekit:prefetch href="/lamp/{lamp}">{lamp}</a>
+	{/each}
+</div>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	div {
+		display: grid;
+		row-gap: 1rem;
+		text-align: center;
 	}
 </style>
---
+++ src/routes/lamp/[lamp].svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { fade } from 'svelte/transition';
+
+	import customFetch from '$lib/customFetch';
+
+	function toggleLight() {
+		customFetch(`/api/lamp/${$page.params.lamp}/toggle-light`);
+	}
+</script>
+
+<svelte:head>
+	<title>Lamp Controller</title>
+</svelte:head>
+
+<!-- check for inspiration n.6 https://1stwebdesigner.com/free-code-snippets-css-buttons/ -->
+
+<button on:click={toggleLight} transition:fade>Toggle Light</button>
