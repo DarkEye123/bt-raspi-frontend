e88e511cf304ce3b73cd7f3dff99a9e45ea989e3
Matej Lesko <lesko.matej.pu@gmail.com>
2021-11-01 12:13:56 +0100

    error component

---
+++ src/lib/Error.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let title = 'Unknown Error';
+</script>
+
+<div>
+	<button on:click>×</button>
+	<h2>{title}</h2>
+	<p><slot /></p>
+</div>
+
+<style>
+	div {
+		background-color: lightcoral;
+		position: absolute;
+		bottom: 0;
+		min-width: 350px;
+		width: 100%;
+		min-height: 5rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	h2 {
+		font-weight: bold;
+	}
+	P {
+		margin-top: 0;
+	}
+	button {
+		position: absolute;
+		right: 0;
+	}
+</style>
