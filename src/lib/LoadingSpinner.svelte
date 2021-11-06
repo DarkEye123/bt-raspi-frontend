5f33269930b71facbb6adf5671ade85bea43d060
Matej Lesko <lesko.matej.pu@gmail.com>
2021-11-06 20:42:48 +0100

    loadding spinner rewritten

--- src/lib/LoadingSpinner.svelte
+++ src/lib/LoadingSpinner.svelte
@@ -1,24 +1,26 @@
+<script lang="ts">
+	export let hasAbsolutePosition = true;
+	export let align: 'right' | 'center' = 'center';
+	export let size: 'normal' | 'small' = 'normal';
+</script>
+
 <!-- taken from https://loading.io/css/ -->
 <div
-	style="
-				display: flex;
-				position: absolute;
-				align-items: center;
-				justify-content: center;
-				"
+	class="lds-ring"
+	style="position: {hasAbsolutePosition ? 'absolute' : 'relative'};"
+	class:center-align={align === 'center'}
+	class:right-align={align === 'right'}
+	class:small={size === 'small'}
 >
-	<div class="lds-ring">
-		<div />
-		<div />
-		<div />
-		<div />
-	</div>
+	<div />
+	<div />
+	<div />
+	<div />
 </div>
 
 <style>
 	.lds-ring {
-		display: flex;
-		position: relative;
+		transform-origin: center;
 		width: 30px;
 		height: 30px;
 	}
@@ -33,6 +35,10 @@
 		border-radius: 50%;
 		animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
 		border-color: #000 transparent transparent transparent;
+	}
+	.small div {
+		width: 25px;
+		height: 25px;
 	}
 	.lds-ring div:nth-child(1) {
 		animation-delay: -0.45s;
@@ -50,5 +56,14 @@
 		100% {
 			transform: rotate(360deg);
 		}
+	}
+	.right-align {
+		right: 15%;
+		transform: translate(-100%, 0%);
+	}
+	.center-align {
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
 	}
 </style>
