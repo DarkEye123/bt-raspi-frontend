5d74f34d9c623771595f83f4a701320317b5d5d5
Matej Lesko <lesko.matej.pu@gmail.com>
2021-11-06 20:42:40 +0100

    button component

---
+++ src/lib/Button.svelte
@@ -0,0 +1,41 @@
+<script>
+	import LoadingSpinner from './LoadingSpinner.svelte';
+
+	export let loading;
+</script>
+
+<button on:click>
+	<div>
+		<div>
+			<slot />
+		</div>
+		{#if loading}
+			<LoadingSpinner />
+		{/if}
+	</div>
+</button>
+
+<style>
+	div {
+		position: relative;
+	}
+	button {
+		display: block;
+		letter-spacing: 0.3ch;
+		text-transform: uppercase;
+		padding: 0.5rem 1rem;
+		cursor: pointer;
+		color: #000;
+		border: none;
+		background: white;
+		text-decoration: none;
+	}
+	button:hover {
+		color: #333333;
+		background: lightgrey;
+	}
+	button:active {
+		color: white;
+		background: black;
+	}
+</style>
